3efe88417b9c6e1a9a17bb6a9675b40218134d1b
Doug Friedman <dfriedm3@gmail.com>
2019-05-29 19:43:55 -0400

    update benchmark script to match original

--- AudioFeature/Sources/bench/main.swift
+++ AudioFeature/Sources/bench/main.swift
@@ -2,27 +2,40 @@ import Foundation
 import CoreFoundation
 import AudioFeature
 
-public func benchmarkTime(f: ()->()) -> Double {
-  f() // warmup
-  let start = CFAbsoluteTimeGetCurrent()
-  for _ in 0..<1 {f()}
-  return (CFAbsoluteTimeGetCurrent()-start)
-}
+var params = FeatureParams()
+params.samplingFreq = 16000;
+params.frameSizeMs = 25;
+params.frameStrideMs = 10;
+params.numFilterbankChans = 20;
+params.lowFreqFilterbank = 0;
+params.highFreqFilterbank = 8000;
+params.numCepstralCoeffs = 13;
+params.lifterParam = 22;
+params.deltaWindow = 2;
+params.accWindow = 2;
+params.zeroMeanFrame = false;
+params.useEnergy = false;
+params.usePower = false;
+params.windowType = WindowType.hanning
 
-public func benchmark(title:String, f:()->()) {
-  let time = benchmarkTime(f:f)
-  print("\(title): \(time) s")
-}
+let mfcc = Mfcc<Float>(params: params)
 
-let filename = "./Tests/data/sa1.wav"
-let (wavInput, _) = loadSound(filename)
-let mfcc = Mfcc<Float>()
+let audioTimeSec = [1, 10, 15, 20, 25, 50, 100]
+let nTimes = 1000
 
-benchmark(title: "mfcc") {
-  var _ = mfcc.apply(on: wavInput)
-}
+print("Benchmark MFCC")
+for t in audioTimeSec {
+  var totalTime = 0.0
+  var input = [Float](repeating: 0.0, count: t * params.samplingFreq)
+  for _ in 0..<nTimes {
+    for i in 0..<input.count {
+      input[i] = Float.random(in: 0..<1)
+    }
 
-let mfsc = Mfsc<Float>()
-benchmark(title: "mfsc") {
-  var _ = mfsc.apply(on: wavInput)
+    let start = CFAbsoluteTimeGetCurrent()
+    let _ = mfcc.apply(on: input)
+    let end = CFAbsoluteTimeGetCurrent()
+    totalTime += (end - start) * 1000
+  }
+  print("| Input Size : \(t) sec , Av. time taken \(totalTime / Double(nTimes)) msec")
 }
